4ee600350091207d4797cb01d2de16c2a055b94c
immortal <ali.hatampouri@gmail.com>
2023-04-29 22:40:38 +0330

    create timer component

---
+++ src/components/Timer/Timer.jsx
@@ -0,0 +1,11 @@
+const Timer = (props) => {
+    return ( 
+        <div>
+            <span>30 s</span>
+            <button>show</button>
+            <button>reset</button>
+        </div>
+     );
+}
+ 
+export default Timer;
