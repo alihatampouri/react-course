1ca09721ead839ebdb0162139f07c6159174f878
immortal <ali.hatampouri@gmail.com>
2023-04-30 07:38:57 +0330

    Styling the timer component

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,20 +1,30 @@
+import { RestartAltOutlined, VisibilityOffOutlined, VisibilityOutlined } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
 import { useEffect, useState } from "react";
 
 const Timer = (props) => {
-  const [status, setStatus] = useState(0);
+  const [status, setStatus] = useState(1);
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     console.log("component did mount");
     const interval = setInterval(() => setCounter((count) => count + 1), 1000);
-    return () => clearInterval(interval)
+    return () => clearInterval(interval);
   }, []);
 
   return (
-    <div>
-      <span>{counter} second</span>
-      <button>show</button>
-      <button>reset</button>
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 my-8">
+      <div className="flex justify-between col-start-2 border rounded-md py-2 px-4 items-center">
+        <span>{status ? `${counter} second` : `Timer hidden`}</span>
+        <div>
+          <IconButton onClick={() => setStatus((current) => !current)}>
+            {status ? <VisibilityOutlined /> : <VisibilityOffOutlined />}
+          </IconButton>
+          <IconButton onClick={() => setCounter(0)}>
+            <RestartAltOutlined />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 };
