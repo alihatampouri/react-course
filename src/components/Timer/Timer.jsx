13d83f0a7c48d26dc4ba2c92e04184d594744315
immortal <ali.hatampouri@gmail.com>
2023-04-30 06:48:15 +0330

    add interval to timer component

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,11 +1,22 @@
+import { useEffect, useState } from "react";
+
 const Timer = (props) => {
-    return ( 
-        <div>
-            <span>30 s</span>
-            <button>show</button>
-            <button>reset</button>
-        </div>
-     );
-}
- 
+  const [status, setStatus] = useState(0);
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log("component did mount");
+    const interval = setInterval(() => setCounter((count) => count + 1), 1000);
+    return () => clearInterval(interval)
+  }, []);
+
+  return (
+    <div>
+      <span>{counter} second</span>
+      <button>show</button>
+      <button>reset</button>
+    </div>
+  );
+};
+
 export default Timer;
