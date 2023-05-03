0a55cf0e2361664d1fc6b50ec06e0ff586bc8871
immortal <ali.hatampouri@gmail.com>
2023-05-03 09:34:18 +0330

    add userContextDispatcher

--- src/components/Context/UserProvider.js
+++ src/components/Context/UserProvider.js
@@ -1,22 +1,31 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState } from "react";
 
 export const UserContext = createContext();
+export const UserContextDispatcher = createContext();
 
 const UserProvider = ({ children }) => {
   const [users, setUsers] = useState([
     {
+      id: 1,
       firstname: "ali",
       lastname: "hatampouri",
       job: "programmer",
     },
     {
+      id: 2,
       firstname: "sobhan",
       lastname: "dolatkhah",
       job: "designer",
     },
   ]);
 
-  return <UserContext.Provider value={users}>{children}</UserContext.Provider>;
+  return (
+    <UserContext.Provider value={users}>
+      <UserContextDispatcher.Provider value={setUsers}>
+        {children}
+      </UserContextDispatcher.Provider>
+    </UserContext.Provider>
+  );
 };
 
 export default UserProvider;
